6b6867a912315fe32b414f93b3e21246396917a3
Xscripts-official <wagkuha6@gmail.com>
2025-08-07 05:19:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-{
-  "version": 2,
-  "builds": [
-    { "src": "index.js", "use": "@vercel/node" }
-  ],
-  "routes": [
-    { "src": "/(.*)", "dest": "/index.js" }
-  ]
-}
+const fs = require("fs");
+const path = require("path");
+
+module.exports = async (req, res) => {
+  const filePath = path.join(__dirname, "files", "main.txt");
+
+  if (!fs.existsSync(filePath)) {
+    res.statusCode = 500;
+    return res.end("main.txt not found");
+  }
+
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  fs.createReadStream(filePath).pipe(res);
+};
