d4eaa12238160121a3678455e9536e922c446a4b
Xscripts-official <wagkuha6@gmail.com>
2025-08-07 05:18:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const fs = require("fs");
-const path = require("path");
-
-module.exports = async (req, res) => {
-  const urlPath = req.url.replace(/^\/raw\//, "");
-  const filePath = path.join(__dirname, "files", urlPath);
-
-  if (!fs.existsSync(filePath)) {
-    res.statusCode = 404;
-    return res.end("File not found");
-  }
-
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  fs.createReadStream(filePath).pipe(res);
-};
+{
+  "version": 2,
+  "builds": [
+    { "src": "index.js", "use": "@vercel/node" }
+  ],
+  "routes": [
+    { "src": "/(.*)", "dest": "/index.js" }
+  ]
+}
